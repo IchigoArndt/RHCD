8e0c6af2ad730f7efe6f08ea0b3fd633367433f9
Luiz Felipe Arndt <luiz.costa@ndd.tech>
2025-05-17 11:26:15 -0300

    Finalização do bot

--- chatbot.js
+++ chatbot.js
@@ -21,7 +21,7 @@ const delay = ms => new Promise(res => setTimeout(res, ms)); // Função que usa
 
 client.on('message', async msg => {
 
-    if (msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|teste)/i) && msg.from.endsWith('@c.us')) {
+    if (msg.body && msg.from.endsWith('@c.us')) {
 
         const chat = await msg.getChat();
 
@@ -38,20 +38,66 @@ client.on('message', async msg => {
         
     }
 
+    if (msg.body !== null && msg.from.endsWith('@c.us'))
+    {
+        let opcao = msg.body;
 
-    if (msg.body !== null && msg.body === '1' && msg.from.endsWith('@c.us')) {
-        const chat = await msg.getChat();
+        switch (opcao) {
+            case '1':
+                opcao1(msg);
+                break;
 
-        //await delay(1000); //delay de 2 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(2000);
-        await client.sendMessage(msg.from, 'Para verificar os eventos que estão ocorrendo, acesse nossa página web: https://www.orionparque.com');
+            case '2':
+                opcao2(msg);
+                break;
+
+            case '3':
+                opcao3(msg);
+                break;
+
+            default:
+                await client.sendMessage(msg.from, 'Ops! Acho que não entendi direito');
+        }
 
     }
 
-    if (msg.body !== null && msg.body === '2' && msg.from.endsWith('@c.us')) {
-        
-        const chat = await msg.getChat();
+    if (msg.body !== null && msg.body === '3' && msg.from.endsWith('@c.us')) {
+    
+    }
+});
+
+// Função para buscar uma sala por ID
+function buscarSalaPorId(id) {
+  // Ler o arquivo JSON
+  const dados = fs.readFileSync('salas.json', 'utf-8');
+
+  // Converter o conteúdo para um objeto JavaScript
+  const salas = JSON.parse(dados);
+
+  // Buscar a sala pelo ID
+  const salaEncontrada = salas.find(sala => sala.id === id);
+
+  // Retornar a sala encontrada ou uma mensagem caso não exista
+  return salaEncontrada ? salaEncontrada : `Sala com ID ${id} não encontrada.`;
+}
+
+function obterTodosOsIds() {
+  const dados = fs.readFileSync('salas.json', 'utf-8');
+  const salas = JSON.parse(dados);
+  return salas.map(sala => sala.id);
+}
+
+async function opcao1(msg)
+{
+    const chat = await msg.getChat();
+    await chat.sendStateTyping(); // Simulando Digitação
+    await delay(2000);
+    await client.sendMessage(msg.from, 'Para verificar os eventos que estão ocorrendo, acesse nossa página web: https://www.orionparque.com');
+}
+
+async function opcao2(msg) 
+{
+     const chat = await msg.getChat();
         const idsDasSalas = obterTodosOsIds();
         await chat.sendStateTyping();
         await delay(1000);
@@ -78,33 +124,54 @@ client.on('message', async msg => {
             await client.sendMessage(msg.from, mensagemFinal);
         }
 
-    }
+        await client.sendMessage(msg.from, 'Caso tenha interesse, deseja agendar qual tipo de sala?\n\n1 - Sala de Reunião ou PodCast\n2 - Sala de Auditório ou SerraLab\n3 - Não tenho interesse.');
+        
+        if(msg.body !== null && msg.from.endsWith('@c.us')) {
+            switch (msg.body) {
+                case '1':
+                    await chat.sendStateTyping(); 
+                    await delay(2000);
+                    await client.sendMessage(msg.from, 'Legal! Iremos te direcionar ao formulário para agendar a sala. \n https://forms.gle/US6KSCpuSVxo7hsy9');
+                    break;
+                case '2':
+                    await chat.sendStateTyping(); 
+                    await delay(2000);
+                    await client.sendMessage(msg.from, 'Legal! Iremos te direcionar ao formulário para agendar a sala. \n plid.in/seueventonoorion');
+                    break;
+                case '3':
+                    await chat.sendStateTyping(); 
+                    await delay(2000);
+                    await client.sendMessage(msg.from, '\n4 - Falar com atendente.\n5 - Encerrar a conversa.');
+                    switch (msg.body) {
+                        case '4':
+                            await chat.sendStateTyping(); 
+                            await delay(2000);
+                            await client.sendMessage(msg.from, 'Certo, estamos transferindo você para um atendente. Por favor, aguarde um momento.');
+                            break;
+                        case '5':
+                            await chat.sendStateTyping(); 
+                            await delay(2000);
+                            await client.sendMessage(msg.from, 'Obrigado por entrar em contato! Se precisar de mais alguma coisa, é só chamar. Até logo!');
+                            break;
+                        default:
+                            await chat.sendStateTyping(); 
+                            await delay(2000);
+                            await client.sendMessage(msg.from, 'Opção inválida. Por favor, digite 4 ou 5.');
+                    }
+                    break;
+                    default:
+                    await chat.sendStateTyping();
+                    await delay(2000);
+                    await client.sendMessage(msg.from, 'Opção inválida. Por favor, digite 1, 2 ou 3.');
+                    break;
+            }
+        }
+}
 
-    if (msg.body !== null && msg.body === '3' && msg.from.endsWith('@c.us')) {
-        const chat = await msg.getChat();
+async function opcao3(msg) 
+{
+     const chat = await msg.getChat();
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(2000);
         await client.sendMessage(msg.from, 'Certo, estamos transferindo você para um atendente. Por favor, aguarde um momento.');
-    }
-});
-
-// Função para buscar uma sala por ID
-function buscarSalaPorId(id) {
-  // Ler o arquivo JSON
-  const dados = fs.readFileSync('salas.json', 'utf-8');
-
-  // Converter o conteúdo para um objeto JavaScript
-  const salas = JSON.parse(dados);
-
-  // Buscar a sala pelo ID
-  const salaEncontrada = salas.find(sala => sala.id === id);
-
-  // Retornar a sala encontrada ou uma mensagem caso não exista
-  return salaEncontrada ? salaEncontrada : `Sala com ID ${id} não encontrada.`;
-}
-
-function obterTodosOsIds() {
-  const dados = fs.readFileSync('salas.json', 'utf-8');
-  const salas = JSON.parse(dados);
-  return salas.map(sala => sala.id);
 }
